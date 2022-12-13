3622eba30c839a8da7de1cc554f243375bb35f3d
Daniel Richter <danielrichter@posteo.de>
2022-12-13 09:03:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-let name = 'http-fetch'
+const name = 'http-fetch'
 
-let setup = ({onResolve, onLoad}) => {
-  onResolve({filter: /^https:\/\//}, resolveFile)
-  onResolve({filter: /.*/, namespace: 'http-fetch'}, resolveUrl)
-  onLoad({filter: /.*/, namespace: 'http-fetch'}, loadSource)
+const setup = ({onResolve, onLoad}) => {
+    onResolve({filter: /^https:\/\//}, resolveFile)
+    onResolve({filter: /.*/, namespace: 'http-fetch'}, resolveUrl)
+    onLoad({filter: /.*/, namespace: 'http-fetch'}, loadSource)
 }
 
-let resolveFile = ({path}) => ({
-  path: path,
-  namespace: 'http-fetch'
+const resolveFile = ({path}) => ({
+    path: path,
+    namespace: 'http-fetch'
 })
 
-let resolveUrl = ({path, importer}) => ({
-  path: new URL(path, importer).href,
-  namespace: 'http-fetch'
+const resolveUrl = ({path, importer}) => ({
+    path: new URL(path, importer).href,
+    namespace: 'http-fetch'
 })
 
-let loadSource = async ({path}) => {
-  let source = await fetch(path)
+const loadSource = async ({path}) => {
+    const source = await fetch(path)
 
-  if (!source.ok) {
-    let message = `GET ${path} failed: status ${source.status}`
-    throw new Error(message)
-  }
+    if (!source.ok) {
+        const message = `GET ${path} failed: status ${source.status}`
+        throw new Error(message)
+    }
 
-  let contents = await source.text()
-  let pattern = /\/\/# sourceMappingURL=(\S+)/
-  let match = contents.match(pattern)
-  if (match) {
-    let url = new URL(match[1], source.url)
-    let dataurl = await loadMap(url)
-    let comment = `//# sourceMappingURL=${dataurl}`
-    contents = contents.replace(pattern, comment)
-  }
+    let contents = await source.text()
+    const pattern = /\/\/# sourceMappingURL=(\S+)/
+    const match = contents.match(pattern)
+    if (match) {
+        const url = new URL(match[1], source.url)
+        const dataurl = await loadMap(url)
+        const comment = `//# sourceMappingURL=${dataurl}`
+        contents = contents.replace(pattern, comment)
+    }
 
-  const allowedLoaders = ['js','ts','tsx','jsx','json','css','text','binary','base64','dataurl','file','copy']
-  let {pathname} = new URL(source.url)
-  let loader = pathname.match(/[^.]+$/)[0]
-  if(!allowedLoaders.includes(loader)){
-      loader = 'ts'
-  }
+    const allowedLoaders = ['js','ts','tsx','jsx','json','css','text','binary','base64','dataurl','file','copy']
+    const {pathname} = new URL(source.url)
+    let loader = pathname.match(/[^.]+$/)[0]
+    if(!allowedLoaders.includes(loader)){
+        loader = 'ts'
+    }
 
-  return {contents, loader}
+    return {contents, loader}
 }
 
-let loadMap = async url => {
-  let map = await fetch(url)
-  let type = map.headers.get('content-type').replace(/\s/g, '')
-  let buffer = await map.arrayBuffer()
-  let blob = new Blob([buffer], {type})
-  let reader = new FileReader()
-  return new Promise(cb => {
-    reader.onload = e => cb(e.target.result)
-    reader.readAsDataURL(blob)
-  })
+const loadMap = async url => {
+    const map = await fetch(url)
+    const type = map.headers.get('content-type').replace(/\s/g, '')
+    const buffer = await map.arrayBuffer()
+    const blob = new Blob([buffer], {type})
+    const reader = new FileReader()
+    return new Promise(cb => {
+        reader.onload = e => cb(e.target.result)
+        reader.readAsDataURL(blob)
+    })
 }
 
 export default {name, setup}
